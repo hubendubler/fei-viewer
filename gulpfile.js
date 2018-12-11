1435d0c3acaa81e593c409aac373748713436054
hubendubler <hubendubler@users.noreply.github.com>
2018-12-11 10:46:56 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+/*jshint node: true, bitwise:true, curly:true, forin:true, noarg:true, noempty:true, nonew:true, undef:true, strict:true, browser:true, node:true, asi:false, es5: true, evil: true, nomen: true */
+"use strict";
+var gulp = require('gulp');  
+var sass = require('gulp-sass');  
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function () {  
+    gulp.src('scss/styles.scss')
+        .pipe(sass({includePaths: ['scss']}))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('browser-sync', function() {  
+    browserSync.init(["css/*.css", "js/*.js"], {
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+    gulp.watch("**/*.js").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function () {  
+    gulp.watch("scss/*.scss", ['sass']);
+});
